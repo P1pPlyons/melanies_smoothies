e3fc84ebc48b640c46a894bba4214fa8409577e5
P1pPlyons <Phillip.Lyons@clarionhg.com>
2024-07-07 09:58:53 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,8 @@ st.title("Customize Your Smoothie:cup_with_straw:")
 st.write("""Choose the fruits you want in your custom Smoothie!""")
 
 name_on_order = st.text_input('Name on Smoothie: ')
-st.write('The name on your Smoothie will be: ',name_on_order)
+#st.write('The name on your Smoothie will be: ',name_on_order)
+fv_df = st.dataframe(data=fruityvice_response.json(), use_container_Width=True_
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
